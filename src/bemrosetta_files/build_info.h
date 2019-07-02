7b6f255e05c886974f73da1722e78b4bf9bbe9a2
IñakiZabala <38589221+izabala123@users.noreply.github.com>
2019-07-02 09:32:59 +0200

    Updates in automatic deployment

--- src/bemrosetta_files/build_info.h
+++ src/bemrosetta_files/build_info.h
@@ -1,9 +1,9 @@
 #define bmYEAR    19
 #define bmMONTH   7
-#define bmDAY     1
-#define bmHOUR    20
-#define bmMINUTE  33
-#define bmSECOND  15
-#define bmTIME    Time(19, 7, 1, 20, 33, 15)
+#define bmDAY     2
+#define bmHOUR    9
+#define bmMINUTE  13
+#define bmSECOND  47
+#define bmTIME    Time(19, 7, 2, 9, 13, 47)
 #define bmMACHINE "aupa-VB"
 #define bmUSER    "aupa"
