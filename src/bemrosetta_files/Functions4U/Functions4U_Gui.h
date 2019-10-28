c76a871c64b87b01833b9b4239e0f3e838db268b
Zabala Calvo <0203853@sener.es>
2019-10-28 12:41:56 +0100

    Improved plot values visualization

--- src/bemrosetta_files/Functions4U/Functions4U_Gui.h
+++ src/bemrosetta_files/Functions4U/Functions4U_Gui.h
@@ -90,7 +90,7 @@ void DrawRectLine(Draw& w, int x, int y, int width, int height, int lineWidth, c
 void DrawRectLine(Draw& w, Point &pos, Size &s, int lineWidth, const Color &color);
 void DrawRectLine(Draw& w, Rect &r, int lineWidth, const Color &color);
 
-int GetEditWidth(const String str);
+Size GetEditSize(const String &_str, const Font &font);
 
 class ConsoleOutput {
 public:
--- src/bemrosetta_files/ScatterCtrl/PopUpText.cpp
+++ src/bemrosetta_files/ScatterCtrl/PopUpText.cpp
@@ -4,21 +4,26 @@ using namespace Upp;
 
 #include "PopUpText.h"
 
-
-void PopUpInfo::Paint(Draw& w)
-{
-	Size sz = GetSize();
-	if(!IsTransparent())
-		w.DrawRect(0, 0, sz.cx, sz.cy, color);
-	PaintLabel(w, 0, 0, sz.cx, sz.cy, !IsShowEnabled(), false, false, VisibleAccessKeys());
+static Size GetEditSize(const String &_str, const Font &font) {
+	WString str(_str);
+	Size ret(0, 0);
+	int retx = 0, nlines = 1;
+	for (int i = 0; i < str.GetCount(); ++i) {
+		int c = str[i];
+		if (c == '\n') {
+			nlines++;
+			ret.cx = max(ret.cx, retx);
+			retx = 0;
+		} else
+			retx += font.GetWidth(c);
+	}
+	ret.cx = max(ret.cx, retx);
+	ret.cy = nlines*font.GetHeight() + font.GetDescent();
+	return ret;
 }
 
-PopUpInfo::PopUpInfo(): color(SColorInfo())
-{
-	Transparent(false);
-	NoWantFocus();
-	IgnoreMouse();
-	SetAlign(ALIGN_CENTER);
-	SetFrame(BlackFrame());
-	opened = false;
+PopUpText &PopUpText::SetText(String _text) {
+	text = _text;		
+	sz = GetEditSize(text, font);
+	return *this;
 }
