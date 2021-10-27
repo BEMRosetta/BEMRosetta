71840cbf63d9e63a342d592c9521e0d04bce4cd9
Iñaki <izabala123@gmail.com>
2021-10-27 17:11:22 +0200

    Fixed hydrostatic forces calculation

--- unittest/Unittest.bat
+++ unittest/Unittest.bat
@@ -2,3 +2,6 @@ call UnittestBEMRosetta.bat CLANGX64
 @IF %ERRORLEVEL% NEQ 0 PAUSE "Error testing and deploying BEMRosetta CLANGX64"
 call UnittestBEMRosetta.bat MSBT19x64
 @IF %ERRORLEVEL% NEQ 0 PAUSE "Error testing and deploying BEMRosetta MSBT19x64"
+cd ..\install
+call make_installer.bat
+@IF %ERRORLEVEL% NEQ 0 PAUSE "Error making the installer"
