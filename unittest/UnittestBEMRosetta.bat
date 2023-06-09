93642d4f2611408be705b1828896f84c49860ac2
Iñaki <izabala123@gmail.com>
2023-06-09 09:31:02 +0200

    Changes in installing. Some bug fixes

--- unittest/UnittestBEMRosetta.bat
+++ unittest/UnittestBEMRosetta.bat
@@ -22,24 +22,21 @@ umk BEMRosetta BEMRosetta    %1 %2 +GUI 			.\.test\BEMRosetta.exe
 umk BEMRosetta BEMRosetta_cl %1 %2 +BEMR_DLL,DLL	.\.test\libbemrosetta.dll
 @IF %ERRORLEVEL% NEQ 0 EXIT /B 1
 
-rem @.\.test\BEMRosetta_cl -paramfile TestBEMRosetta_CL.txt
-rem @IF %ERRORLEVEL% NEQ 0 EXIT /B 1
-
 @title Compiling BEMRosetta_cl TEST_DLL %1
 @echo Compiling BEMRosetta_cl TEST_DLL %1
 umk BEMRosetta BEMRosetta_cl %1 %2 +BEMR_TEST_DLL	.\.test\testdll_bemrosetta.exe
 @IF %ERRORLEVEL% NEQ 0 EXIT /B 1
 .\.test\testdll_bemrosetta.exe  .\.test ..
  
-@title Copying BEMRosetta %1
-@echo Copying BEMRosetta %1
-copy .\.test\BEMRosetta.exe ..\bin
-@IF %ERRORLEVEL% NEQ 0 EXIT /B 1
-copy .\.test\BEMRosetta_cl.exe ..\bin
-@IF %ERRORLEVEL% NEQ 0 EXIT /B 1
-copy .\.test\libbemrosetta.dll ..\bin
-@IF %ERRORLEVEL% NEQ 0 EXIT /B 1
-copy .\.test\libbemrosetta.txt ..\bin
-@IF %ERRORLEVEL% NEQ 0 EXIT /B 1
-copy .\.test\libbemrosetta.py ..\bin
-@IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+rem @title Copying BEMRosetta %1
+rem @echo Copying BEMRosetta %1
+rem copy .\.test\BEMRosetta.exe ..\_bin
+rem @IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+rem copy .\.test\BEMRosetta_cl.exe ..\_bin
+rem @IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+rem copy .\.test\libbemrosetta.dll ..\_bin
+rem @IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+rem copy .\.test\libbemrosetta.txt ..\_bin
+rem @IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+rem copy .\.test\libbemrosetta.py ..\_bin
+rem @IF %ERRORLEVEL% NEQ 0 EXIT /B 1
